5d498ae5d69ace1026d711bbd21340b71db16b77
davidsikoralabs <davidsikoralabs@gmail.com>
2026-03-10 19:37:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,49 +3,8 @@ const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 const fs = require("fs");
+
 const messagesFile = path.join(__dirname, "messages.json");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-io.on("connection", (socket) => {
-  console.log("Uživatel se připojil");
-
-  // Pošleme historii novému uživateli
-  socket.emit("chatHistory", messages);
-
-  socket.on("setName", (name) => {
-    socket.username = name || "Anonym";
-  });
-
-  socket.on("sendMessage", (text) => {
-    if (!socket.username) socket.username = "Anonym";
-
-    const msg = {
-      user: socket.username,
-      text,
-      time: new Date().toLocaleTimeString()
-    };
-
-    messages.push(msg);
-    saveMessages(messages); // ← uložíme do JSON souboru
-
-    io.emit("receiveMessage", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Uživatel se odpojil");
-  });
-});
-
-
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server běží na http://localhost:${PORT}`);
-});
 
 function loadMessages() {
   try {
@@ -65,6 +24,57 @@ function saveMessages(messages) {
   }
 }
 
+function getRandomColor() {
+  const colors = [
+    "#e6194b", "#3cb44b", "#ffe119", "#383434",
+    "#f58231", "#911eb4", "#46f0f0", "#72013d",
+    "#bcf60c", "#f5500e", "#008080", "#e300f7",
+    "#9a6324", "#8b0000", "#ff0707", "#13f055",
+    "#808000", "#3d43f3", "#000075", "#808080"
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
 let messages = loadMessages();
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("Uživatel se připojil");
+
+  socket.emit("chatHistory", messages);
+
+  socket.on("setName", (name) => {
+    socket.username = name || "Anonym";
+    socket.color = getRandomColor();
+  });
+
+  socket.on("sendMessage", (text) => {
+    if (!socket.username) socket.username = "Anonym";
+
+    const msg = {
+      user: socket.username,
+      text,
+      time: new Date().toLocaleTimeString(),
+      color: socket.color
+    };
+
+    messages.push(msg);
+    saveMessages(messages);
+
+    io.emit("receiveMessage", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Uživatel se odpojil");
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server běží na http://localhost:${PORT}`);
+});
