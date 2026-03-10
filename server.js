48a8729c3b0d4f43469f4e37009712ab962d85b3
davidsikoralabs <davidsikoralabs@gmail.com>
2026-03-10 12:20:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server běží na http://localhost:${PORT}`);
 });
+
