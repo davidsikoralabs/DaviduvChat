f164af6f7cab1da50f6269efc11b86c658cb1cac
davidsikoralabs <davidsikoralabs@gmail.com>
2026-03-23 16:53:11 +0100

    update

---
+++ public/pages/login.js
@@ -0,0 +1,29 @@
+console.log("LOGIN JS LOADED");
+
+import { supabase } from "/supabase.js";
+
+document.getElementById("loginBtn").onclick = async () => {
+    console.log("CLICK WORKS");
+
+    const email = document.getElementById("loginEmail").value.trim();
+    const password = document.getElementById("loginPassword").value.trim();
+
+    if (!email || !password) {
+        alert("Vyplňte email i heslo.");
+        return;
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    console.log("SIGNIN RESULT:", data, error);
+
+    if (error) {
+        alert("Chyba: " + error.message);
+        return;
+    }
+
+    goTo("/rooms");
+};
