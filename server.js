0d94c556a38b91060f987092096d9ec67238f7da
davidsikoralabs <davidsikoralabs@gmail.com>
2026-03-24 14:57:28 +0100

    update

--- server.js
+++ server.js
@@ -270,5 +270,5 @@ function updateRoomUserCount(roomId) {
   io.emit("roomUserCount", { roomId, count });
 }
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Server běží na portu", PORT));
