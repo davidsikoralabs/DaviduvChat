999dbf29fa53fec5ef115bc558800e7922a61ca0
davidsikoralabs <davidsikoralabs@gmail.com>
2026-04-01 18:12:18 +0200

    update

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const io = new Server(server, {
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 let rooms = [];
